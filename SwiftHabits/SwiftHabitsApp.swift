faef90a82047c9031b348db4008474b093b3c2b3
Linus Ilbratt <linusilbratt@gmail.com>
2024-04-22 10:02:17 +0200

    first commit

---
+++ SwiftHabits/SwiftHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftHabitsApp.swift
+//  SwiftHabits
+//
+//  Created by Linus Ilbratt on 2024-04-22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
