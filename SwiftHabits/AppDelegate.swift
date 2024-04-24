a6232fd2d22d34be0100e051ef46c3dfbbe81fd8
Linus Ilbratt <linusilbratt@gmail.com>
2024-04-24 12:20:33 +0200

    creating AppDelegate file and setting up request for notifications

---
+++ SwiftHabits/AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  SwiftHabits
+//
+//  Created by Linus Ilbratt on 2024-04-24.
+//
+
+import UIKit
+import SwiftUI
+
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Set the notification center delegate
+        UNUserNotificationCenter.current().delegate = self
+        
+        // Request notification permissions
+        requestNotificationPermission()
+        
+        return true
+    }
+    
+    private func requestNotificationPermission() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                print("Notification permission granted.")
+            } else if let error = error {
+                print("Notification permission error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    // Handle notifications while app is in the foreground
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound])
+    }
+    
+    // Handle response to notifications
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+}
+
+
--- SwiftHabits/SwiftHabitsApp.swift
+++ SwiftHabits/SwiftHabitsApp.swift
@@ -6,12 +6,16 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 @main
 struct SwiftHabitsApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
